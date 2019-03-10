1cdddc901dc848ca6ed650aefadcf9dbac732cce
Origami ONOUE <onoueorigami@gmail.com>
2019-03-11 00:29:16 +0900

    はい

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+	mode: "production",
+	entry: "./src/index.ts",
+	output: {
+		filename: "dist/bundle.js",
+		library: "dynamizr",
+		libraryTarget: "umd"
+	},
+	resolve: {
+		// Add `.ts` and `.tsx` as a resolvable extension.
+		extensions: [".ts", ".tsx", ".js"]
+	},
+	module: {
+		rules: [
+			// all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+			{ test: /\.tsx?$/, loader: "ts-loader" }
+		]
+	}
+};
